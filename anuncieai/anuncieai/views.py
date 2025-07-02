063ac1540d3b60a84c0d892a3568e40c07288024
arthconf <arthur-confessor@hotmail.com>
2025-07-02 16:06:05 -0400

    correcoes pagina principal

--- anuncieai/anuncieai/views.py
+++ anuncieai/anuncieai/views.py
@@ -1,17 +1,19 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from django.db.models import Count
+from categoria.models import Categoria  # Corrigido o import
 from produtos.models import Produto
-
 
 def visualizarHome(request):
     produtos = Produto.objects.all().filter(esta_disponivel=True)
-
+    categorias = Categoria.objects.annotate(
+        produto_count=Count('produto')
+    ).all()
+    
+    # Combine ambos produtos e categorias em um único context
     context = {
-        'produtos' : produtos
+        'produtos': produtos,
+        'categorias': categorias
     }
 
     return render(request, 'home.html', context)
-
-    
-
